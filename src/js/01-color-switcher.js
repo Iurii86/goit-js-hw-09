06cd20b82565e66b57934fe1bc3200fce6c247b5
Iurii Sydorov <iurii.sydorov@gmail.com>
2023-02-14 11:15:45 +0200

    hw-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let changeBodyColor = null;
 
+
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    changeBodyColor = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+};
+
+function onStopBtn() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(changeBodyColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
